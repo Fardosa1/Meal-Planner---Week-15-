7f6b184066d4097be1e8d609aa703dd9e2901e73
Fardowsaa <fardowsaa1@Fardowsaas-MacBook-Air.local>
2023-04-10 06:06:03 -0500

    Code with Notes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+// Let's talk about my meal planner app! 
+// Proivdes a bunch on different meals for people to spice up their kitchen
+// Using a mockAPI Sponacualr Api
+
+// This portion of the code defines the api_key which is 
+// is used to fetcch request to retreive meals from Spoonacular
+// The component renders a MealForm component to allow users to add new meals 
+
+
+//to the planner. It also maps over the meals array and renders a Meal component 
+//for each meal in the planner. The onDelete and onEdit functions are passed down to each Meal component 
+//to handle removing and editing meals from the planner..
+
+
+
+
 import { useState, useEffect } from 'react';
 import './App.css';
 import Meal from './Meal';
--- src/Meal.js
+++ src/Meal.js
@@ -1,3 +1,18 @@
+// Meal Planning App To Provide Varaties 
+
+// This portion of the code exports a functional component "meal" that 
+// receives props, meal, onDelete and onEdit
+// Using the useState hook to defind a booolean statee
+// isediting that is set to false at fitst. 
+
+
+// The component renders either a MealForm component or a div with meal
+// information depending on the value of isEditing. If isEditing is true, MealForm is rendered to allow the user to edit the meal. 
+
+// Otherwise, the meal's image, title, and description are rendered along with two buttons to edit or delete the meal.
+// When the delete button is clicked, onDelete function is called with the meal object's id. When the edit button is clicked, t
+//he isEditing state is set to true. If the MealForm component's onSave prop is called with an updated meal object, onEdit function is called 
+//with the meal object's id and the updated meal object, and isEditing is set to false.
 import { useState } from 'react';
 import MealForm from './MealForm';
 import './Meal';
